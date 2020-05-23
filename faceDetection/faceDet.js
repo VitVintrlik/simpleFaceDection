20fbd61fee605a33c16e033eefdcaa3c9270922c
VitVintrlik <60343316+VitVintrlik@users.noreply.github.com>
2020-05-23 23:48:43 +0200

    Initial

---
+++ faceDetection/faceDet.js
@@ -0,0 +1,34 @@
+const video = document.getElementById('vid');
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/aps/faceDetection/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/aps/faceDetection/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/aps/faceDetection/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/aps/faceDetection/models'),
+]).then(startVid)
+
+function startVid()
+{
+    navigator.getUserMedia(
+        {video: {} },
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    )
+}
+
+video.addEventListener('play', () => 
+{
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = {width: video.width, height:video.height}
+    faceapi.matchDimensions(canvas, displaySize) 
+    setInterval(async () => {
+        const detection = await faceapi.detectAllFaces(video, 
+        new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+        const resizedDetection = faceapi.resizeResults(detection, displaySize)
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetection)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetection)
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetection)
+    }, 100)
+})
